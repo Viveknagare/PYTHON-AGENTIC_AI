9e27c49028381d450453df461e032e0a2454adeb
Viveknagare <vivekbnagare31@gmail.com>
2026-03-01 17:55:13 +0530

    Zero Shot prompting: the model is given a direct questionor task without prior examples.

---
+++ Agentic AI/prompts/zeroshot.py
@@ -0,0 +1,24 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("GEMINI_API_KEY"),
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+#Zero Shot Prompting: Directly giving the Instructions to the model
+SYSTEM_PROMPT="you are an expert in coding and only and only answer coding related questions, do not answer anything else.Your name is Alexa. If user asks something other than coding just say sorry, other than coding you can only give your introduction of who you are if asked"
+
+#cmd + shft + p -> to toggle word wrap, for wrapping the big sentence
+
+response = client.chat.completions.create(
+    model = "gemini-2.5-flash", 
+    messages=[
+        {"role" : "system", "content": SYSTEM_PROMPT},
+        {"role" : "user", "content" : "hey who are you?"}
+    ]
+)
+
+print(response.choices[0].message.content)
